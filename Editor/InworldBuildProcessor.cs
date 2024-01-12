95d4653708597522084e7c139c4d6b49b629f22e
Yan Jin <601746725@qq.com>
2024-01-11 18:19:26 -0800

    add log

--- Editor/InworldBuildProcessor.cs
+++ Editor/InworldBuildProcessor.cs
@@ -22,6 +22,7 @@ namespace Inworld
         {
             AssetDatabase.importPackageCompleted += async packageName =>
             {
+                Debug.Log("Importing with InworldAI.");
                 if (InworldAI.Initialized)
                     return;
                 _AddDebugMacro();
