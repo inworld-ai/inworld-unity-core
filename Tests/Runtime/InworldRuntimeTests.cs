ba87dace33b732706c440e7f511d788e1d78c442
Yan Jin <601746725@qq.com>
2024-01-17 17:26:50 -0800

    Add test framework

---
+++ Tests/Runtime/InworldRuntimeTests.cs
@@ -0,0 +1,26 @@
+﻿/*************************************************************************************************
+ * Copyright 2022-2024 Theai, Inc. dba Inworld AI
+ *
+ * Use of this source code is governed by the Inworld.ai Software Development Kit License Agreement
+ * that can be found in the LICENSE.md file or at https://www.inworld.ai/sdk-license
+ *************************************************************************************************/
+
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.TestTools;
+namespace Inworld.Test
+{
+	public class InworldRuntimeTests
+	{
+		[UnityTest]
+		public IEnumerator InworldRuntimeTest_P()
+		{
+			Object.Instantiate(InworldAI.ControllerPrefab);
+			Assert.NotNull(InworldController.Instance);
+			Object.DestroyImmediate(InworldController.Instance);
+			Assert.IsNull(InworldController.Instance);
+			yield return null;
+		}
+	}
+}
