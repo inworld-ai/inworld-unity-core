5101b5a8b7256055ee99665acea73aa542ba3788
Yan Jin <601746725@qq.com>
2024-02-29 11:17:55 -0800

    INTG-1292 refactor splash screen

--- Runtime/Scripts/Data/Entities/InworldEnum.cs
+++ Runtime/Scripts/Data/Entities/InworldEnum.cs
@@ -11,12 +11,9 @@ namespace Inworld
     {
         Idle, // Initial state
         Initializing, // Used at getting runtime token.
-        // InitFailed, // Getting runtime Token failed.
         Initialized, // Getting runtime Token Completed. 
-        LoadingSceneCompleted, // Used in legacy NDK only. 
         Connecting, // Start Session with Inworld Server by runtime token.
         Connected, // Controller is connected to World-Engine and ready to work.
-        // LostConnect, // Received when session expired without error, i.e, due to inactivity
         Exhausted, // Received when user is running out of quota.
         Error // Some error occured.
     }
---
+++ Runtime/Scripts/Sample/StatusPanel.cs
@@ -0,0 +1,35 @@
+﻿/*************************************************************************************************
+ * Copyright 2022-2024 Theai, Inc. dba Inworld AI
+ *
+ * Use of this source code is governed by the Inworld.ai Software Development Kit License Agreement
+ * that can be found in the LICENSE.md file or at https://www.inworld.ai/sdk-license
+ *************************************************************************************************/
+
+using TMPro;
+using UnityEngine;
+
+namespace Inworld.Sample
+{
+	public class StatusPanel: MonoBehaviour
+	{
+		[SerializeField] GameObject m_Board;
+		[SerializeField] TMP_Text m_Indicator;
+		
+		void OnEnable()
+		{
+			InworldController.Client.OnStatusChanged += OnStatusChanged;
+		}
+
+		void OnDisable()
+		{
+			if (!InworldController.Instance)
+				return;
+			InworldController.Client.OnStatusChanged -= OnStatusChanged;
+		}
+		void OnStatusChanged(InworldConnectionStatus incomingStatus)
+		{
+			m_Board.SetActive(incomingStatus != InworldConnectionStatus.Idle && incomingStatus != InworldConnectionStatus.Connected);
+			m_Indicator.text = incomingStatus.ToString();
+		}
+	}
+}
