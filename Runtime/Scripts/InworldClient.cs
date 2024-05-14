d5acc4b890ea3302871fc2a1641a6b85e2d979ec
Yan Jin <601746725@qq.com>
2024-05-14 13:51:27 -0700

    delete the errorly generated code by rider

--- Runtime/Scripts/InworldClient.cs
+++ Runtime/Scripts/InworldClient.cs
@@ -5,7 +5,6 @@
  * that can be found in the LICENSE.md file or at https://www.inworld.ai/sdk-license
  *************************************************************************************************/
 
-using Codice.CM.Common;
 using Inworld.Packet;
 using Inworld.Entities;
 using Inworld.Interactions;
